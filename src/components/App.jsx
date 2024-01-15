f5c2fc4b9904516f0e4bce488ee5cfbb18f9d07a
Tetiana-UA <izidaxm85@gmail.com>
2024-01-15 16:18:33 +0100

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,9 @@
+import { Profile } from "./Profile/Profile";
+import user from "../data/user.json";
+
+
+
+
 export const App = () => {
   return (
     <div
@@ -10,6 +16,13 @@ export const App = () => {
         color: '#010101'
       }}
     >
+      <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+/>
       React homework template
     </div>
   );
